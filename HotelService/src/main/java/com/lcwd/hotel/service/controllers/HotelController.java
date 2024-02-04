875635473bcc5346c9e8fe8d0447b1c5dd475294
AC96825 <kumarmanish9502@gmail.com>
2024-02-04 22:05:22 +0530

    Implemented single signin with JWT using Okta OAuth2

--- HotelService/src/main/java/com/lcwd/hotel/service/controllers/HotelController.java
+++ HotelService/src/main/java/com/lcwd/hotel/service/controllers/HotelController.java
@@ -5,6 +5,7 @@ import com.lcwd.hotel.service.services.HotelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,18 +18,21 @@ public class HotelController {
     //create hotel
 
     @PostMapping()
+    @PreAuthorize("hasAuthority('Admin')")
     public ResponseEntity<Hotel> createHotel(@RequestBody Hotel hotel){
         return ResponseEntity.status(HttpStatus.CREATED).body(hotelService.createHotel(hotel));
     }
 
     //get single Hotel
     @GetMapping("/{hotelId}")
+    @PreAuthorize("hasAuthority('SCOPE_internal')")
     public ResponseEntity<Hotel> getHotelById(@PathVariable("hotelId") String hotelId){
         return ResponseEntity.status(HttpStatus.OK).body(hotelService.getHotelById(hotelId));
     }
 
     //get all hotels
     @GetMapping()
+    @PreAuthorize("hasAuthority('SCOPE_internal') || hasAuthority('Admin')")
     public ResponseEntity<List<Hotel>> getAllHotels(){
         return ResponseEntity.status(HttpStatus.OK).body(hotelService.getAllHotels());
     }
